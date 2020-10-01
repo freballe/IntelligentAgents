6a33014368ca24234fed80c742404bbe3f9dbeb3
Anzuoni Elia <elianzuoni@gmail.com>
2020-10-01 23:06:33 +0200

    logs added

--- reactive/src/template/ReactiveTemplate.java
+++ reactive/src/template/ReactiveTemplate.java
@@ -16,13 +16,13 @@ public class ReactiveTemplate implements ReactiveBehavior {
 	private Agent agent;
 	private Topology topo;
 	private TaskDistribution td;
-	
+
 	private int numActions;
-	
+
 	private double gamma;	// The discount factor
 	private State states[][][];	// The 3D array of all possible states, indexed by (vehicle, currCity, destCity)
 	// Since all task involve two distinct cities, the triple (v,i,i) is used to code a state where no task is available
-	
+
 
 	@Override
 	public void setup(Topology topology, TaskDistribution td, Agent agent) {
@@ -37,14 +37,14 @@ public class ReactiveTemplate implements ReactiveBehavior {
 		this.td = td;
 		this.numActions = 0;
 		this.gamma = discount;
-		
+
 
 		// Check that sum over j is 1 for every i
 		printTaskDistribution();
-		
+
 		// Pre-construct all states
 		constructStates();
-		
+
 		// Reinforcement learning
 		learnOptimalStrategy();
 
@@ -57,13 +57,13 @@ public class ReactiveTemplate implements ReactiveBehavior {
 		// currCityID = destCityID <==> no task is available
 		int currCityID = vehicle.getCurrentCity().id;
 		int destCityID = (availableTask == null) ? currCityID : availableTask.deliveryCity.id;
-		
+
 		// The state holds the information about the best action to do (learnt with RL)
 		State currState = states[vehicle.id()][currCityID][destCityID];
 		// An action is coded as the best city to move to (null means to accept the task)
 		City bestAction = currState.getBestAction();
 
-		
+
 		if(bestAction == null) {
 			// We trust the RL algorithm, and don't check whether availableTask == null
 			action = new Pickup(availableTask);
@@ -72,51 +72,81 @@ public class ReactiveTemplate implements ReactiveBehavior {
 			action = new Move(bestAction);
 		}
 		
+		// Log the task reward
+		if(availableTask != null) {
+			if(availableTask.reward != td.reward(vehicle.getCurrentCity(), availableTask.deliveryCity)) {
+				System.out.println("**** PROBLEM: ACTUAL REWARD != EXPECTED REWARD ****");
+			}
+		}
+
 
 		if (numActions >= 1) {
 			System.out.println("The total profit after " + numActions + " actions is " + 
-								agent.getTotalProfit() + " (average profit: " + 
-								(agent.getTotalProfit() / (double)numActions) + ")");
+					agent.getTotalProfit() + " (average profit: " + 
+					(agent.getTotalProfit() / (double)numActions) + ")");
 		}
 		numActions++;
 
 		return action;
 	}
-	
+
 
 	/** 
 	 * Prints out the distribution.
 	 * Check that, on each row, the sum is one, and that the diagonal is 0. 
+	 * Check that the task rewards stay the same over multiple launches.
 	 */
 	private void printTaskDistribution() {
+		// Probability distribution
+		System.out.println("\nProbability distribution\n");
+
 		// Column names
 		for(int i=-1; i < topo.size(); i++) {
 			System.out.printf("\t %d", i);
 		}
 		System.out.println("\t Sum");
-		
+
 		// Rows
 		for(City from : topo) {
 			double sum = 0.0;
 			// Row name
 			System.out.printf("%d \t", from.id);
-			
+
 			// Null destination
 			System.out.printf("%.3f   ", td.probability(from, null));
 			sum += td.probability(from, null);
-			
+
 			// Other cities
 			for(City to : topo) {
 				System.out.printf("%.3f   ", td.probability(from, to));
 				sum += td.probability(from, to);
 			}
-			
+
 			// Sum
 			System.out.printf("%.3f\n", sum);
 		}
-		
+
+		// Task reward
+		System.out.println("\nTask reward\n");
+
+		// Column names
+		for(int i=0; i < topo.size(); i++) {
+			System.out.printf("\t %d", i);
+		}
+
+		// Rows
+		for(City from : topo) {
+			// Row name
+			System.out.printf("\n%d      ", from.id);
+
+			// Cities
+			for(City to : topo) {
+				System.out.printf("%5d   ", td.reward(from, to));
+			}
+		}
+
 		System.out.print("\n\n");
-		
+
 		return;
 	}
 
@@ -126,7 +156,7 @@ public class ReactiveTemplate implements ReactiveBehavior {
 	 */
 	private void constructStates() {
 		states = new State[agent.vehicles().size()][topo.size()][topo.size()];
-		
+
 		for(Vehicle vehicle : agent.vehicles()) {
 			for(City curr : topo.cities()) {
 				for(City dest : topo.cities()) {
@@ -136,64 +166,64 @@ public class ReactiveTemplate implements ReactiveBehavior {
 				}
 			}
 		}
-		
+
 		return;
 	}
-	
+
 	/**
 	 * Implements the Value Iteration algorithm in this setting.
 	 */
 	private void learnOptimalStrategy() {
-		
+
 		// TODO: do value iteration with eps-delta
 		for(Vehicle vehicle : agent.vehicles()) {
 			// Iterate over vehicles
-			
+
 			for(int curr=0; curr < topo.size(); curr++) {
 				for(int dest=0; dest < topo.size(); dest++) {
 					// Iterate over states
-					
+
 					State state = states[vehicle.id()][curr][dest];
 					double bestQLO = - Double.MAX_VALUE;	// The new V(s), initialised to -inf
 					City bestAction = null;
-					
+
 					for(City action : state.possibleActions()) {
 						// Iterate over possible actions
-						
+
 						double qlo = state.reward(action);	// The Q(s,a) in the slides
-						
+
 						int nextCurr = state.getNextCity(action).id;	// All the possible next states share the same currCity
 						for(int nextDest=0; nextDest < topo.size(); nextDest++) {
 							// Iterate over possible next states
-							
+
 							State nextState = states[vehicle.id()][nextCurr][nextDest];
 							qlo += gamma * nextState.getProb() * nextState.getValue();
-							
+
 							// End of iteration over possible next states
 						}
-						
+
 						// Update bestQLO and bestAction
 						if(qlo > bestQLO) {
 							bestQLO = qlo;
 							bestAction = action;
 						}
-						
+
 						// End of iteration over possible actions
 					}
-					
+
 					// TODO: get variation in V(s) and compare with eps
 					// Update value and bestAction in state
 					state.setValue(bestQLO);
 					state.setBestAction(bestAction);
-					
+
 					// End of iteration over states
 				}
 			}
-			
+
 			// End of iteration over vehicles
 		}
-		
+
 		return;
 	}
-	
+
 }
