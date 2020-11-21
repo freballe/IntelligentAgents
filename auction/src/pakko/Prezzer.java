4bd811d6ec58752044492d1cd24e91c958344662
freballe <francesco.ballestrazzi@epfl.ch>
2020-11-21 15:12:21 +0100

    cane

--- auction/src/pakko/Prezzer.java
+++ auction/src/pakko/Prezzer.java
@@ -31,17 +31,48 @@ public class Prezzer {
 		interestRatio = minRatio;
 	}
 
+	//1
+		
 	void setParameters() {
 		// Reads the user-chosen default marginal cost from the configuration file
 		defaultMarginalCost = agent.readProperty("def-marg-cost", Long.class, 300L);
 		factorVehicles = agent.readProperty("vehicles-factor", Integer.class, 2);		
 		// Reads the user-chosen default marginal cost from the configuration file
 		minRatio = agent.readProperty("min-ratio", Double.class, 0.05);
-		maxRatio = agent.readProperty("max-ratio", Double.class, 0.50);
+		maxRatio = agent.readProperty("max-ratio", Double.class, 0.5);
 		ratioIncreaseRate = agent.readProperty("ratio-increase", Double.class, 0.2);
 		ratioDecreaseRate = agent.readProperty("ratio-decrease", Double.class, 0.1);
 
 	}
+	
+	
+	//2
+	/*
+	void setParameters() {
+		// Reads the user-chosen default marginal cost from the configuration file
+		defaultMarginalCost = agent.readProperty("def-marg-cost", Long.class, 300L);
+		factorVehicles = agent.readProperty("vehicles-factor", Integer.class, 2);		
+		// Reads the user-chosen default marginal cost from the configuration file
+		minRatio = agent.readProperty("min-ratio", Double.class, 0.01);
+		maxRatio = agent.readProperty("max-ratio", Double.class, 1.0);
+		ratioIncreaseRate = agent.readProperty("ratio-increase", Double.class, 0.5);
+		ratioDecreaseRate = agent.readProperty("ratio-decrease", Double.class, 0.3);
+	}
+	*/
+	//3
+	/*
+	void setParameters() {
+		// Reads the user-chosen default marginal cost from the configuration file
+		defaultMarginalCost = agent.readProperty("def-marg-cost", Long.class, 300L);
+		factorVehicles = agent.readProperty("vehicles-factor", Integer.class, 2);		
+		// Reads the user-chosen default marginal cost from the configuration file
+		minRatio = agent.readProperty("min-ratio", Double.class, 0.1);
+		maxRatio = agent.readProperty("max-ratio", Double.class, 0.3);
+		ratioIncreaseRate = agent.readProperty("ratio-increase", Double.class, 0.1);
+		ratioDecreaseRate = agent.readProperty("ratio-decrease", Double.class, 0.05);
+	}
+	*/
+
 
 	private boolean isFirstPhase() {
 		return nWonTasks == null || nWonTasks[agent.id()] < (factorVehicles * vehicles.size());
@@ -91,13 +122,16 @@ public class Prezzer {
 	}
 
 	public Long askPrice(double marginalCost) {
+		if(isFirstPhase()) {
+			if(marginalCost < 0) {
+				marginalCost = 0;
+			}
+			return Math.round(marginalCost + 1);
+		}
+		
 		// Default marginal cost if <= 0
 		if(marginalCost < defaultMarginalCost) {
 			marginalCost = defaultMarginalCost;
-		}
-
-		if(isFirstPhase()) {
-			return Math.round(marginalCost + 1);
 		}
 
 		// Compute price to ask
